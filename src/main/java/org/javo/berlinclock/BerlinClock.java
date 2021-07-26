1a0046a7fa2d7de701c770cccdd3feb8c85b2415
Jerson <jrsonv@gmail.com>
2021-07-25 20:02:36 -0500

    All tests pased

--- src/main/java/org/javo/berlinclock/BerlinClock.java
+++ src/main/java/org/javo/berlinclock/BerlinClock.java
@@ -5,7 +5,6 @@ import io.smallrye.mutiny.infrastructure.Infrastructure;
 import lombok.extern.slf4j.Slf4j;
 
 import javax.enterprise.context.ApplicationScoped;
-import java.util.Arrays;
 import java.util.regex.Matcher;
 import java.util.regex.Pattern;
 
@@ -14,14 +13,18 @@ import java.util.regex.Pattern;
 public class BerlinClock {
 
 
-    private final String DIGITAL_TIME_REGEX = "([01]?[0-9]|2[0-3]):[0-5][0-9]:[0-5][0-9]";
-
-    public Uni<String> convertDigitalTimeToBerlinTime(String digitalTime){
+    public Uni<String> convertDigitalTimeToBerlinTime(String digitalTime) {
         return Uni.createFrom().item(() -> digitalTime)
-                .onItem().invoke(this::validateFormat);
+                .onItem().invoke(this::validateFormat)
+                .onItem().transform(s -> calculateSecondsRow(s).await().indefinitely() +
+                        calculateFiveHoursRow(s).await().indefinitely() +
+                        calculateSingleHoursRow(s).await().indefinitely() +
+                        calculateFiveMinutesRow(s).await().indefinitely() +
+                        calculateSingleMinutesRow(s).await().indefinitely()
+                );
     }
 
-    public Uni<String> validateFormat(String digitalTime){
+    public Uni<String> validateFormat(String digitalTime) {
         return Uni.createFrom().item(digitalTime)
                 .runSubscriptionOn(Infrastructure.getDefaultWorkerPool())
                 .onItem().invoke(this::verifyDigitalTimeRegex)
@@ -32,109 +35,133 @@ public class BerlinClock {
 
     }
 
-    public Uni<String> calculateSingleMinutesRow(String digitalTime){
+    public Uni<String> calculateSingleMinutesRow(String digitalTime) {
         return Uni.createFrom().item(() -> splitDigitalTime(digitalTime, 2))
                 .onItem().transform(this::computeNumberOfLampsInSingleMinutesRow)
                 .onItem().transform(number -> generateSingleMinutesRowPattern(number, 4));
     }
 
-    public Uni<String> calculateFiveMinutesRow(String digitalTime){
+    public Uni<String> calculateFiveMinutesRow(String digitalTime) {
         return Uni.createFrom().item(() -> splitDigitalTime(digitalTime, 2))
                 .onItem().transform(this::computeNumberOfLampsInFiveMinutesRow)
                 .onItem().transform(number -> generateFiveMinutesRowPattern(number, 11));
     }
 
-    public Uni<String> calculateSingleHoursRow(String digitalTime){
+    public Uni<String> calculateSingleHoursRow(String digitalTime) {
         return Uni.createFrom().item(() -> splitDigitalTime(digitalTime, 1))
                 .onItem().transform(this::computeNumberOfLampsInSingleHoursRow)
                 .onItem().transform(number -> generateSingleHoursRowPattern(number, 4));
     }
 
-    private Integer computeNumberOfLampsInSingleMinutesRow(String timeSegment){
+    public Uni<String> calculateFiveHoursRow(String digitalTime) {
+        return Uni.createFrom().item(() -> splitDigitalTime(digitalTime, 1))
+                .onItem().transform(this::computeNumberOfLampsInFiveHoursRow)
+                .onItem().transform(number -> generateFiveHoursRowPattern(number, 4));
+    }
+
+    public Uni<String> calculateSecondsRow(String digitalTime) {
+        return Uni.createFrom().item(() -> splitDigitalTime(digitalTime, 3))
+                .onItem().transform(this::generateSecondsRowPattern);
+    }
+
+
+    private Integer computeNumberOfLampsInSingleMinutesRow(String timeSegment) {
         return calculateModule(convertTimeSegmentToInteger(timeSegment), 5);
     }
 
-    private Integer computeNumberOfLampsInFiveMinutesRow(String timeSegment){
+    private Integer computeNumberOfLampsInFiveMinutesRow(String timeSegment) {
         return calculateQuotient(convertTimeSegmentToInteger(timeSegment), 5);
     }
 
-    private Integer computeNumberOfLampsInSingleHoursRow(String timeSegment){
+    private Integer computeNumberOfLampsInSingleHoursRow(String timeSegment) {
         return calculateModule(convertTimeSegmentToInteger(timeSegment), 5);
     }
 
-    private String generateSingleMinutesRowPattern(int numberOfY, int rowLength){
-        /*String yChars = yCharGenerator(numberOfY);
-        log.info("yChars: " + yChars);
-        String oChars = oCharGenerator(rowLength-numberOfY);
-        log.info("oChars: " + oChars);
-        return yChars+oChars;*/
-
-        return yCharGenerator(numberOfY) + oCharGenerator(rowLength-numberOfY);
+    private Integer computeNumberOfLampsInFiveHoursRow(String timeSegment) {
+        return calculateQuotient(convertTimeSegmentToInteger(timeSegment), 5);
     }
 
-    private String generateFiveMinutesRowPattern(int numberOfY, int rowLength){
+    private String generateSingleMinutesRowPattern(int numberOfY, int rowLength) {
+        return yCharGenerator(numberOfY) + oCharGenerator(rowLength - numberOfY);
+    }
+
+    private String generateFiveMinutesRowPattern(int numberOfY, int rowLength) {
         String partialResult = yCharGenerator(numberOfY);
-        return replaceYCharsForRChars(partialResult, 3, 2) + oCharGenerator(rowLength-numberOfY);
+        return replaceYCharsForRChars(partialResult, 3, 2) + oCharGenerator(rowLength - numberOfY);
     }
 
-    private String generateSingleHoursRowPattern(int numberOfR, int rowLength){
-        return rCharGenerator(numberOfR) + oCharGenerator(rowLength-numberOfR);
+    private String generateSingleHoursRowPattern(int numberOfR, int rowLength) {
+        return rCharGenerator(numberOfR) + oCharGenerator(rowLength - numberOfR);
     }
 
-    private String replaceYCharsForRChars(String partialResult,Integer positionMultiplier, Integer positionOffset){
+    private String generateFiveHoursRowPattern(int numberOfR, int rowLength) {
+        return rCharGenerator(numberOfR) + oCharGenerator(rowLength - numberOfR);
+    }
+
+    private String generateSecondsRowPattern(String digitalTime) {
+        String result;
+        if (convertTimeSegmentToInteger(digitalTime) % 2 == 0)
+            result = yCharGenerator(1);
+        else
+            result = oCharGenerator(1);
+        return result;
+    }
+
+    private String replaceYCharsForRChars(String partialResult, Integer positionMultiplier, Integer positionOffset) {
         char[] yCharArray = partialResult.toCharArray();
-        int numberOfYChars = partialResult.length()/3;
+        int numberOfYChars = partialResult.length() / 3;
         char[] rCharArray = rCharGenerator(numberOfYChars).toCharArray();
-        for (int rCharCounter = 0; rCharCounter < rCharArray.length; rCharCounter++ ){
-            yCharArray[(rCharCounter*positionMultiplier)+positionOffset] = rCharArray[rCharCounter];
+        for (int rCharCounter = 0; rCharCounter < rCharArray.length; rCharCounter++) {
+            yCharArray[(rCharCounter * positionMultiplier) + positionOffset] = rCharArray[rCharCounter];
         }
         return String.valueOf(yCharArray);
     }
 
-    private String oCharGenerator(int numberOfcharsO){
+    private String oCharGenerator(int numberOfcharsO) {
         StringBuilder stringBuilder = new StringBuilder(numberOfcharsO);
-        for(int charOCounter = 0; charOCounter < numberOfcharsO; charOCounter++){
+        for (int charOCounter = 0; charOCounter < numberOfcharsO; charOCounter++) {
             stringBuilder.insert(charOCounter, "O");
         }
         return stringBuilder.toString();
     }
 
-    private String yCharGenerator(int numberOfcharsY){
+    private String yCharGenerator(int numberOfcharsY) {
         StringBuilder stringBuilder = new StringBuilder(numberOfcharsY);
-        for(int charOCounter = 0; charOCounter < numberOfcharsY; charOCounter++){
+        for (int charOCounter = 0; charOCounter < numberOfcharsY; charOCounter++) {
             stringBuilder.insert(charOCounter, "Y");
         }
         return stringBuilder.toString();
     }
 
-    private String rCharGenerator(int numberOfcharsR){
+    private String rCharGenerator(int numberOfcharsR) {
         StringBuilder stringBuilder = new StringBuilder(numberOfcharsR);
-        for(int charOCounter = 0; charOCounter < numberOfcharsR; charOCounter++){
+        for (int charOCounter = 0; charOCounter < numberOfcharsR; charOCounter++) {
             stringBuilder.insert(charOCounter, "R");
         }
         return stringBuilder.toString();
     }
 
-    private String splitDigitalTime(String digitalTime, int segment){
-        return digitalTime.split(":")[segment-1];
+    private String splitDigitalTime(String digitalTime, int segment) {
+        return digitalTime.split(":")[segment - 1];
     }
 
-    private String verifyDigitalTimeRegex(String digitalTime){
+    private void verifyDigitalTimeRegex(String digitalTime) {
+        String DIGITAL_TIME_REGEX = "([01]?[0-9]|2[0-3]):[0-5][0-9]:[0-5][0-9]";
         Pattern pattern = Pattern.compile(DIGITAL_TIME_REGEX);
         Matcher matcher = pattern.matcher(digitalTime);
         if (!matcher.matches())
             throw new AssertionError("The format is not valid");
-        return digitalTime;
     }
 
-    private Integer convertTimeSegmentToInteger(String timeSegment){
+    private Integer convertTimeSegmentToInteger(String timeSegment) {
         return Integer.parseInt(timeSegment);
     }
 
-    private Integer calculateModule(int dividend, int divisor){
+    private Integer calculateModule(int dividend, int divisor) {
         return dividend % divisor;
     }
-    private Integer calculateQuotient(int dividend, int divisor){
+
+    private Integer calculateQuotient(int dividend, int divisor) {
         return dividend / divisor;
     }
 
--- src/test/java/org/javo/berlinclock/BerlinClockTest.java
+++ src/test/java/org/javo/berlinclock/BerlinClockTest.java
@@ -21,14 +21,14 @@ public class BerlinClockTest {
     }
 
     @ParameterizedTest(name = " {index} ==> Time ''{0}''")
-    @ValueSource(strings = { "00:00:00", "23:59:59", "12:32:00", "12:34:00", "12:35:00" })
+    @ValueSource(strings = {"00:00:00", "23:59:59", "12:32:00", "12:34:00", "12:35:00"})
     void checkValidateFormatWithCorrectFormats(String time) {
         String result = underTest.validateFormat(time).await().indefinitely();
         assertEquals(result, time);
     }
 
     @ParameterizedTest(name = " {index} ==> Time ''{0}''")
-    @ValueSource(strings = { "00", "assdas", "1asds1", "", "12:35:0" })
+    @ValueSource(strings = {"00", "assdas", "1asds1", "", "12:35:0"})
     void checkValidateFormatWithIncorrectFormats(String time) {
         assertThrows(IllegalArgumentException.class,
                 () -> underTest.validateFormat(time).await().indefinitely());
@@ -41,31 +41,60 @@ public class BerlinClockTest {
     }
 
     @ParameterizedTest(name = "{index} ==> Time ''{0}'' Row {1}")
-    @CsvSource({ "00:00:00, OOOO", "23:59:59, YYYY", "12:32:00, YYOO", "12:34:00,YYYY","12:35:00, OOOO" })
-    public void checkSingleMinutesRow(String time, String row){
+    @CsvSource({"00:00:00, OOOO", "23:59:59, YYYY", "12:32:00, YYOO", "12:34:00,YYYY", "12:35:00, OOOO"})
+    public void checkSingleMinutesRow(String time, String row) {
         String result = underTest.calculateSingleMinutesRow(time).await().indefinitely();
         assertEquals(row, result);
     }
 
     @ParameterizedTest(name = "{index} ==> Time ''{0}'' Row {1}")
-    @CsvSource({    "00:00:00, OOOOOOOOOOO",
-                    "23:59:59, YYRYYRYYRYY",
-                    "12:04:00, OOOOOOOOOOO",
-                    "12:23:00, YYRYOOOOOOO",
-                    "12:35:00, YYRYYRYOOOO" })
-    public void checkFiveMinutesRow(String time, String row){
+    @CsvSource({"00:00:00, OOOOOOOOOOO",
+            "23:59:59, YYRYYRYYRYY",
+            "12:04:00, OOOOOOOOOOO",
+            "12:23:00, YYRYOOOOOOO",
+            "12:35:00, YYRYYRYOOOO"})
+    public void checkFiveMinutesRow(String time, String row) {
         String result = underTest.calculateFiveMinutesRow(time).await().indefinitely();
         assertEquals(row, result);
     }
 
     @ParameterizedTest(name = "{index} ==> Time ''{0}'' Row {1}")
-    @CsvSource({    "00:00:00, OOOO",
+    @CsvSource({"00:00:00, OOOO",
             "23:59:59, RRRO",
             "02:04:00, RROO",
             "08:23:00, RRRO",
-            "14:35:00, RRRR" })
-    public void checkSingleHoursRow(String time, String row){
+            "14:35:00, RRRR"})
+    public void checkSingleHoursRow(String time, String row) {
         String result = underTest.calculateSingleHoursRow(time).await().indefinitely();
+        assertEquals(row, result);
+    }
+
+    @ParameterizedTest(name = "{index} ==> Time ''{0}'' Row {1}")
+    @CsvSource({"00:00:00, OOOO",
+            "23:59:59, RRRR",
+            "02:04:00, OOOO",
+            "08:23:00, ROOO",
+            "16:35:00, RRRO"})
+    public void checkFiveHoursRow(String time, String row) {
+        String result = underTest.calculateFiveHoursRow(time).await().indefinitely();
+        assertEquals(row, result);
+    }
+
+    @ParameterizedTest(name = "{index} ==> Time ''{0}'' Row {1}")
+    @CsvSource({"00:00:00, Y",
+            "23:59:59, O",})
+    public void checkSecondsRow(String time, String row) {
+        String result = underTest.calculateSecondsRow(time).await().indefinitely();
+        assertEquals(row, result);
+    }
+
+    @ParameterizedTest(name = "{index} ==> Time ''{0}'' Row {1}")
+    @CsvSource({"00:00:00, YOOOOOOOOOOOOOOOOOOOOOOO",
+            "23:59:59, ORRRRRRROYYRYYRYYRYYYYYY",
+            "16:50:06, YRRROROOOYYRYYRYYRYOOOOO",
+            "11:37:01, ORROOROOOYYRYYRYOOOOYYOO",})
+    public void checkEntireBerlinClock(String time, String row) {
+        String result = underTest.convertDigitalTimeToBerlinTime(time).await().indefinitely();
         assertEquals(row, result);
     }
 
